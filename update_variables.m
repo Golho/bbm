0f995b4b3861eee6dc4bb424634e7a6016403da5
NilsVD <nvoigtdahl@gmail.com>
2018-10-23 13:44:20 +0200

    further work

--- update_variables.m
+++ update_variables.m
@@ -5,8 +5,23 @@ function [sigma, dlambda, ep_eff] = update_variables(sigma_old, ep_eff_old, delt
 % ep_eff_old - The effective platic strain in the LCPE
 % delta_eps - The change in total strain
 % Dstar - The linear elastic stiffness matrix
-% mp - Material parameters 
+% mp - Material parameters [F, G, H, L, k, n]
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
+
+F = mp(1);
+G = mp(2);
+H = mp(3);
+L = mp(4);
+k = mp(5);
+n = mp(6);
+
+sigma_y0 = sqrt(3/(2*(F + G + H)));
+
+P = [F+G, -F, -G, 0;
+    -F, F+H, -H, 0;
+    -G, -H, G+H, 0;
+    0, 0, 0, 2*L];
+
 end
 
